0408a2311fe3dc53df6a783ede0688a04a6117cd
abby-moana <abbymoana1@gmail.com>
2023-01-15 20:28:25 +0800

    for statement

--- binary-search-tree.py
+++ binary-search-tree.py
@@ -52,6 +52,9 @@ def build_tree(elements):
     print("Building tree with these elements: ", elements)
     root = BinarySearchTreeNode(elements[0])
 
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+
 
 
 
